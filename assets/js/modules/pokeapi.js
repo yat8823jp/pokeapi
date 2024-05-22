c5fc3f2b39ab750c08a587d9667b3aaa05b4121a
y_kawanishi <yat@yat-net.com>
2024-05-22 22:21:03 +0900

    add-get_datas

--- assets/js/modules/pokeapi.js
+++ assets/js/modules/pokeapi.js
@@ -1,32 +1,6 @@
 const BASE_URL = 'https://pokeapi.co/api/v2/';
-const result_en = document.getElementById( 'result_en' );
-const result_jp = document.getElementById( 'result_jp' );
-const submit_en = document.querySelector( '.js-submit_en' );
-const submit_jp = document.querySelector( '.js-submit_jp' );
-
-async function get_pokemon_japanese_name( get_english_name ) {
-	try {
-		const response = await fetch( BASE_URL + `pokemon-species/${ get_english_name.toLowerCase() }` );
-		if ( ! response.ok ) {
-			throw new Error( "Japanese name acquisition error" );
-		}
-		const data = await response.json();
-		console.log(data);
-		for( let index in data.names ) {
-			const language = data.names[index]['language']['name'];
-			const set_japanese_name = data.names[index]['name'];
-			if ( language == 'ja-Hrkt' ) {
-				result_en.textContent = set_japanese_name;
-				return set_japanese_name;
-			} else {
-				return 'Not Found Japanese name';
-			}
-		}
-	} catch ( error ) {
-		result_en.textContent = '日本語名が見つかりませんでした';
-		console.error( "There has been a problem with your fetch operation:", error );
-	}
-}
+const result = document.getElementById( 'result' );
+const submit = document.querySelector( '.js-submit' );
 
 async function get_pokemon_english_name( get_japanese_name ) {
 	try {
@@ -41,19 +15,42 @@ async function get_pokemon_english_name( get_japanese_name ) {
 			return pokemon_list[ pokemon_index ];
 		};
 		const pokemon_name = name_list( get_japanese_name );
-		result_jp.textContent =  pokemon_name.en;
+		result.textContent =  pokemon_name.en;
+		get_pokemon_data( pokemon_name.en );
+
+		return pokemon_name.en;
 	} catch ( error ) {
 		console.error( "Read error for json file" );
 	}
 }
 
-submit_en.addEventListener( 'click', () => {
-	const get_english_name = document.getElementById( 'pokemon_name_en' ).value;
-	get_pokemon_japanese_name( get_english_name );
-} );
+async function get_pokemon_data( english_name ) {
+	try {
+		const response = await fetch( BASE_URL + `pokemon/${ english_name.toLowerCase() }` );
+		if ( ! response.ok ) {
+			throw new Error( "Pokemon data acquisition error" );
+		}
+		const data = await response.json();
+		console.log( data );
+		const versions =data.game_indices.map( index => index.version.name )
+		if ( data ) {
+			document.querySelector( '#app' ).innerHTML =
+				`
+					<p>No.：${ data.id }</p>
+					<p>重量：${ data.weight }</p>
+					<p>登場作品</p>
+					<ul>
+						<li>${ `${ versions.join("</li><li>") }` }</li>
+					</ul>
+				`;
+		}
+		return data;
+	} catch ( error ) {
+		console.error( "There has been a problem with your fetch operation:", error );
+	}
+}
 
-
-submit_jp.addEventListener( 'click', () => {
+submit.addEventListener( 'click', () => {
 	const get_japanese_name = document.getElementById( 'pokemon_name_jp' ).value;
 	get_pokemon_english_name( get_japanese_name );
 } );
